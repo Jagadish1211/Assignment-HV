9c7a6d0274d34032598e7a7567455565bc3dc538
Jagadish1211 <mrjagadish1211@gmail.com>
2025-01-04 02:06:56 +0530

    feat: Create Dashboard component

---
+++ src/Components/Dashboard/index.js
@@ -0,0 +1,53 @@
+import "./styles.scss";
+import { useSelector } from "react-redux";
+
+const Dashboard = () => {
+    const trainDashboardInfo = useSelector((state) => state.trainSchedule.trainDashboardInfo);
+
+    const formatTimeDataForDashboard = (time) => {
+        if(!time) return '-';
+        const date = new Date(time);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+
+        return `${hours}:${minutes}`;
+    }
+
+  return (
+    <div className="train-dashboard">
+      <h1>Dashboard</h1>
+      <div>
+        <table>
+          <tr>
+            <th>Train No</th>
+            <th>Priority</th>
+            <th>Scheduled Arrival Time</th>
+            <th>Actual Arrival Time</th>
+            <th>Scheduled Departure Time</th>
+            <th>Actual Departure Time</th>
+            <th>Delay in Arrival</th>
+            <th>Delay in Departure</th>
+          </tr>
+
+            {trainDashboardInfo?.map(({ trainNumber, priority, arrivalTime, actualArrivalTime, departureTime, actualDepartureTime, delayInArrival, delayInDeparture }) => {
+                return (
+                    <tr>
+                        <td>{trainNumber}</td>
+                        <td>{priority}</td>
+                        <td>{formatTimeDataForDashboard(arrivalTime)}</td>
+                        <td>{formatTimeDataForDashboard(actualArrivalTime)}</td>
+                        <td>{formatTimeDataForDashboard(departureTime)}</td>
+                        <td>{formatTimeDataForDashboard(actualDepartureTime)}</td>
+                        <td>{delayInArrival ?? '-'}</td>
+                        <td>{delayInDeparture ?? '-'}</td>
+                    </tr>
+                );
+            })}
+
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
