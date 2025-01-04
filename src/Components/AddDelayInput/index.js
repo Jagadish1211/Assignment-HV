321a81efdb34af64cd50567420351eba4aa3eb98
Jagadish1211 <mrjagadish1211@gmail.com>
2025-01-04 18:09:25 +0530

    Format new arrival time in schedule card

--- src/Components/AddDelayInput/index.js
+++ src/Components/AddDelayInput/index.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { saveTrainScheduleAction, saveTrainDashboardInfoAction } from "../../features/trainSchedule/trainSchedule";
+import {
+  saveTrainScheduleAction,
+  saveTrainDashboardInfoAction,
+} from "../../features/trainSchedule/trainSchedule";
 
 const AddDelayInput = ({ trainNumber }) => {
   // update schedule and dashboard with the latest arrival time
@@ -20,58 +23,72 @@ const AddDelayInput = ({ trainNumber }) => {
 
   const handleAddDelayToTrain = (trainNumber) => {
     // have the train number and time to be added
-    if(!/^\d{2}:\d{2}$/.test(delayTimeString)) {
-      console.log("Please check format for entering delay")
-      return
-    } 
+    if (!/^\d{2}:\d{2}$/.test(delayTimeString)) {
+      console.log("Please check format for entering delay");
+      return;
+    }
 
-    setDelayTimeString('')
+    setDelayTimeString("");
 
-    let newArrivalTime 
-    let newArrivalTimeString
+    let newArrivalTime;
+    let newArrivalTimeString;
 
-    const updatedTrainSchedule = trainSchedule.map(train => {
-        if(train.trainNumber === trainNumber) {
+    const updatedTrainSchedule = trainSchedule.map((train) => {
+      if (train.trainNumber === trainNumber) {
+        const calculateNewArrivalTime = (time) => {
+          const getCurrentArrivalTime = new Date(time).getTime();
 
-            const calculateNewArrivalTime = (time) => {
-                const getCurrentArrivalTime = new Date(time).getTime();
+          const [delayHours, delayMinutes] = delayTimeString
+            .split(":")
+            .map((item) => Number(item));
+          const totalDelayInMs = (delayHours * 60 + delayMinutes) * 60 * 1000;
 
-                const [delayHours, delayMinutes] = delayTimeString.split(":").map(item => Number(item));
-                const totalDelayInMs = (delayHours * 60  + delayMinutes) * 60 * 1000 ;
+          const newArrivalTimeInISOString = new Date(
+            totalDelayInMs + getCurrentArrivalTime
+          );
+          const now = new Date();
 
-                const newArrivalTimeInISOString = new Date(totalDelayInMs + getCurrentArrivalTime);
-                const now = new Date();
+          console.log(delayTimeString);
 
-                console.log(delayTimeString)
+          return new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            newArrivalTimeInISOString.getHours(),
+            newArrivalTimeInISOString.getMinutes()
+          ).toISOString();
+        };
 
-                return new Date(
-                    now.getFullYear(),
-                    now.getMonth(),
-                    now.getDate(),
-                    newArrivalTimeInISOString.getHours(),
-                    newArrivalTimeInISOString.getMinutes()
-                  ).toISOString();
+        newArrivalTime = calculateNewArrivalTime(train.arrivalTime);
 
-            }
+        const tempNewArrivalDateObject = new Date(newArrivalTime);
+        newArrivalTimeString = `${
+          tempNewArrivalDateObject.getHours() < 10
+            ? `0${tempNewArrivalDateObject.getHours()}`
+            : tempNewArrivalDateObject.getHours()
+        }:${tempNewArrivalDateObject.getMinutes() < 10 ? `0${tempNewArrivalDateObject.getMinutes()}` : tempNewArrivalDateObject.getMinutes()}`;
 
-            newArrivalTime = calculateNewArrivalTime(train.arrivalTime);
+        return {
+          ...train,
+          arrivalTime: newArrivalTime,
+          arrivalTimeString: newArrivalTimeString,
+        };
+      }
 
-            const tempNewArrivalDateObject = new Date(newArrivalTime)
-            newArrivalTimeString = `${tempNewArrivalDateObject.getHours() < 10 ? `0${tempNewArrivalDateObject.getHours()}` : tempNewArrivalDateObject.getHours()}:${tempNewArrivalDateObject.getMinutes()}`
+      return train;
+    });
 
-            return {...train, arrivalTime : newArrivalTime, arrivalTimeString : newArrivalTimeString }
-        }
+    const updatedTrainDashboardInfo = trainDashboardInfo.map((train) => {
+      if (train.trainNumber === trainNumber) {
+        return {
+          ...train,
+          arrivalTime: newArrivalTime,
+          arrivalTimeString: newArrivalTimeString,
+        };
+      }
 
-        return train
-    })
-
-    const updatedTrainDashboardInfo = trainDashboardInfo.map(train => {
-        if(train.trainNumber === trainNumber) {
-            return { ...train, arrivalTime: newArrivalTime, arrivalTimeString : newArrivalTimeString }
-        }
-
-        return train
-    })
+      return train;
+    });
 
     dispatch(saveTrainScheduleAction(updatedTrainSchedule));
     dispatch(saveTrainDashboardInfoAction(updatedTrainDashboardInfo));
