772d9c96a6cc6ed5d4358a5d1927452a8434876a
Jagadish1211 <mrjagadish1211@gmail.com>
2025-01-04 17:56:04 +0530

    feat: Add delay in arrival feature

---
+++ src/Components/AddDelayInput/index.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { saveTrainScheduleAction, saveTrainDashboardInfoAction } from "../../features/trainSchedule/trainSchedule";
+
+const AddDelayInput = ({ trainNumber }) => {
+  // update schedule and dashboard with the latest arrival time
+  const dispatch = useDispatch();
+  const [delayTimeString, setDelayTimeString] = useState("");
+
+  const trainDashboardInfo = useSelector(
+    (state) => state.trainSchedule.trainDashboardInfo
+  );
+  const trainSchedule = useSelector(
+    (state) => state.trainSchedule.trainSchedule
+  );
+
+  const handleTimeStringEntry = (e) => {
+    setDelayTimeString(e.target.value);
+  };
+
+  const handleAddDelayToTrain = (trainNumber) => {
+    // have the train number and time to be added
+    if(!/^\d{2}:\d{2}$/.test(delayTimeString)) {
+      console.log("Please check format for entering delay")
+      return
+    } 
+
+    setDelayTimeString('')
+
+    let newArrivalTime 
+    let newArrivalTimeString
+
+    const updatedTrainSchedule = trainSchedule.map(train => {
+        if(train.trainNumber === trainNumber) {
+
+            const calculateNewArrivalTime = (time) => {
+                const getCurrentArrivalTime = new Date(time).getTime();
+
+                const [delayHours, delayMinutes] = delayTimeString.split(":").map(item => Number(item));
+                const totalDelayInMs = (delayHours * 60  + delayMinutes) * 60 * 1000 ;
+
+                const newArrivalTimeInISOString = new Date(totalDelayInMs + getCurrentArrivalTime);
+                const now = new Date();
+
+                console.log(delayTimeString)
+
+                return new Date(
+                    now.getFullYear(),
+                    now.getMonth(),
+                    now.getDate(),
+                    newArrivalTimeInISOString.getHours(),
+                    newArrivalTimeInISOString.getMinutes()
+                  ).toISOString();
+
+            }
+
+            newArrivalTime = calculateNewArrivalTime(train.arrivalTime);
+
+            const tempNewArrivalDateObject = new Date(newArrivalTime)
+            newArrivalTimeString = `${tempNewArrivalDateObject.getHours() < 10 ? `0${tempNewArrivalDateObject.getHours()}` : tempNewArrivalDateObject.getHours()}:${tempNewArrivalDateObject.getMinutes()}`
+
+            return {...train, arrivalTime : newArrivalTime, arrivalTimeString : newArrivalTimeString }
+        }
+
+        return train
+    })
+
+    const updatedTrainDashboardInfo = trainDashboardInfo.map(train => {
+        if(train.trainNumber === trainNumber) {
+            return { ...train, arrivalTime: newArrivalTime, arrivalTimeString : newArrivalTimeString }
+        }
+
+        return train
+    })
+
+    dispatch(saveTrainScheduleAction(updatedTrainSchedule));
+    dispatch(saveTrainDashboardInfoAction(updatedTrainDashboardInfo));
+  };
+
+  return (
+    <>
+      <input
+        name="delayTime"
+        onChange={(e) => handleTimeStringEntry(e)}
+        type="text"
+        id={`add-delay-${trainNumber}`}
+      ></input>
+
+      <button onClick={() => handleAddDelayToTrain(trainNumber)}>
+        Add delay
+      </button>
+    </>
+  );
+};
+
+export default AddDelayInput;
--- src/Components/ScheduleCard/index.js
+++ src/Components/ScheduleCard/index.js
@@ -1,18 +1,23 @@
-import './styles.scss';
+import "./styles.scss";
+import AddDelayInput from "../AddDelayInput";
 
 const ScheduleCard = ({ trainSchedule }) => {
 
-    return (
-        <div className='train-schedule-card'>
-            { trainSchedule?.map(({ trainNumber, priority, arrivalTime, arrivalTimeString}) => {
-                return (
-                    <div className='train-schedule-card-train' key={trainNumber}>
-                        { `${trainNumber} - ${arrivalTimeString} - ${priority}` }
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="train-schedule-card">
+    <div className="train-schedule-card-delay-instruction">Add delay time in the form of "hh:mm"</div>
+      {trainSchedule?.map(
+        ({ trainNumber, priority, arrivalTime, arrivalTimeString }) => {
+          return (
+            <div className="train-schedule-card-train" key={trainNumber}>
+              {`${trainNumber} - ${arrivalTimeString} - ${priority}`}{" "}
+              <span><AddDelayInput trainNumber={trainNumber}/></span>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
+};
 
 export default ScheduleCard;
